b6818d9c910afa25f4cbe0e095fdebd4e218ddb5
insecurity22 <insecurity@naver.com>
2025-03-28 09:15:00 +0900

    fix: Change homepage url and fixed build error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/front_5th_chapter1-1/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+  homepage: "https://insecurity22.github.io/front_5th_chapter1-1/",
   test: {
     globals: true,
     environment: "jsdom",
