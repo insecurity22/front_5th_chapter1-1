1d7bf470cfd449467a3e13c43bd9648eef51805e
insecurity22 <insecurity@naver.com>
2025-03-31 08:06:21 +0900

    Updates

--- assets/main-CK1T06Vm.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import"./main-Cgph998X.js";function a(){const t=window.location.hash.substring(1);history.replaceState(null,null,t)}a();
---
+++ assets/main-DshuZeQQ.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import"./main-Cgph998X.js";function a(){const t=window.location.pathname,n=window.location.hash.substring(1),o=t+n;history.replaceState(null,null,o)}a();
