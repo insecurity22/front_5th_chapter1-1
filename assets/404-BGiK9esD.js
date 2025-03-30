bfbb056dfa3962ae0cb4f9e057d46b163c688711
insecurity22 <insecurity@naver.com>
2025-03-31 06:54:30 +0900

    Updates

---
+++ assets/404-BGiK9esD.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";function o(){const t=`${location.pathname}/#${path}`;window.location.replace(t)}o();
--- assets/404-Cu4a1xCI.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";const t="/front_5th_chapter1-1/",a=`${t}/#${location.pathname}`;window.location.replace(a);
