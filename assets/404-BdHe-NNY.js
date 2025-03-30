c730cb29be5ad4d8ca63311dcf5b4a65b2fdff1e
insecurity22 <insecurity@naver.com>
2025-03-31 07:07:47 +0900

    Updates

--- assets/404-BdHe-NNY.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";function r(){var c;const t="/front_5th_chapter1-1/",e=(c=location.pathname)==null?void 0:c.replace(t,""),o=`${t}/#${e}`;window.location.replace(o)}r();
